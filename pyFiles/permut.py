9a319c177538962c4591df0255ab6f93a32b3084
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-04-06 12:04:41 -0300

    Update permut.py

--- pyFiles/permut.py
+++ pyFiles/permut.py
@@ -1,3 +1,4 @@
+# define the "factorial" function
 def factorial(value):
     result = 1
     for n in range(value, 0, -1):
@@ -6,25 +7,34 @@ def factorial(value):
     return result
 
 
+# Read keyboard for get the word
 word = str(input("Write the word: ")).upper()
+
+# Create the dictonary and the list
 letters_count = {}
 repetition = []
 
-for l in word:
+# Read the word to find each letter and its number
+for letter in word:
 
-    if l not in letters_count:
-        letters_count[l] = 1
+    if letter not in letters_count:
+        letters_count[letter] = 1
 
     else:
-        letters_count[l] += 1
+        letters_count[letter] += 1
 
+# add repeated letters to "repetiton" list
 for k, v in letters_count.items():
     if v > 1:
         repetition.append(v)
 
+# Define the denominator
 div = 1
 for number in repetition:
     div *= factorial(number)
 
+# Compute the anagrams number
 final = factorial(len(word)) / div
+
+# Display the number of anagrams
 print(f"The total of permutations is: {final}")
